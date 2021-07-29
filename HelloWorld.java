768306e4afb586c351ea4ab99d4acf60a4c5752a
Avinash <boiniavi445@gmail.com>
2021-07-29 12:51:30 +0000

    web-hooks

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, baby");
-    System.out.println("Bye........")
+    System.out.println("Bye........");
   }
 }
